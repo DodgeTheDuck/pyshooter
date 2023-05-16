781f2438f78ed11e8f206f50fd7d962bc35cd267
DaniLangley <125893464+DaniLangley@users.noreply.github.com>
2023-05-16 07:29:27 +0100

    Update main.py
    PEP8 details a python style guide

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import engine
 
 EXIT_SUCCESS = 0
 
+
 def main():    
     engine.init()
     engine.push_state(GameState())
     engine.run()
     return EXIT_SUCCESS
+
 
 if __name__ == '__main__':
     main()
